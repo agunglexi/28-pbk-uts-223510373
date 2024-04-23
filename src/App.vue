2951ad8270cb8e269e044efe298cc03d9353eae7
agunglexi <agungsiak707@gmail.com>
2024-04-23 22:02:45 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <ToDoList />
+</template>
+
+<script>
+import ToDoList from "@/components/ToDoList";
+
+export default {
+  name: 'App',
+
+  components: {
+    ToDoList
+  }
+}
+</script>
