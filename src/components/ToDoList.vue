4ff0e93a35451bb7a67764f9662e67a572b8ec2e
223510373-Agung Lexsi Kurniawan <165940725+agunglexi@users.noreply.github.com>
2024-05-13 18:19:20 +0700

    Update ToDoList.vue

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -2,8 +2,18 @@
   <div class="container-fluid" style="width: 40em">
     <div class="card m-3">
       <div class="card-header">
-        <form @submit.prevent="addTodo">
-          <div class=col-12>
+        <ul class="nav nav-tabs">
+          <li class="nav-item" @click="selectedMenu = 'Todos'" :class="{ 'active': selectedMenu === 'Todos' }">
+            <a class="nav-link" href="#">Todos</a>
+          </li>
+          <li class="nav-item" @click="selectedMenu = 'Post'" :class="{ 'active': selectedMenu === 'Post' }">
+            <a class="nav-link" href="#">Post</a>
+          </li>
+        </ul>
+      </div>
+      <div class="card-body">
+        <div v-if="selectedMenu === 'Todos'">
+          <form @submit.prevent="addTodo">
             <div class="row">
               <div class="col-3">
                 <label for="newTodo" class="form-label">New Todo</label>
@@ -17,40 +27,48 @@
                 </button>
               </div>
             </div>
+          </form>
+
+          <!-- Filter Buttons -->
+          <div class="float-end mt-3">
+            <button class="btn btn-primary btn-sm me-1" @click="toggleFilter('done')" type="button" name="button">
+              Checked
+            </button>
+            <button class="btn btn-primary btn-sm me-1" @click="toggleFilter('undone')" type="button" name="button">
+              Unchecked
+            </button>
+            <button class="btn btn-primary btn-sm" @click="clearFilter" type="button" name="button">
+              Clear
+            </button>
           </div>
-        </form>
-      </div>
-      <div class="card-body">
-        <div class="float-end">
-          <button class="btn btn-success btn-sm me-1" @click="allDone" type="button" name="button">
-            <i class="bi bi-check-all"></i>
-          </button>
-          <button class="btn btn-danger btn-sm me-1" @click="allRemove" type="button" name="button">
-            <i class="bi bi-trash-fill"></i>
-          </button>
-          <!-- New Buttons for Filter -->
-          <button class="btn btn-primary btn-sm me-1" @click="toggleFilter('done')" type="button" name="button">
-            Checked
-          </button>
-          <button class="btn btn-primary btn-sm me-1" @click="toggleFilter('undone')" type="button" name="button">
-            Unchecked
-          </button>
-          <button class="btn btn-primary btn-sm" @click="clearFilter" type="button" name="button">
-            Clear
-          </button>
+
+          <!-- Todo List -->
+          <ul class="list-group list-group-flush list-group-numbered">
+            <li class="list-group-item" v-for="todo in filteredTodos" :key="todo.title">
+              <input class="form-check-input" type="checkbox" v-model="todo.done" id="flexCheckDefault">
+              <label class="form-check-label ms-3" for="flexCheckDefault" :class="{ done: todo.done }">
+                {{ todo.title }}
+              </label>
+              <button @click="removeTodo(todo)" class="btn btn-danger btn-sm float-end" type="button" name="button">
+                <i class="bi bi-trash-fill"></i>
+              </button>
+            </li>
+          </ul>
+        </div>
+
+        <div v-if="selectedMenu === 'Post'">
+          <label for="userSelect">Select User:</label>
+          <select id="userSelect" v-model="selectedUser">
+            <option v-for="user in users" :key="user.id" :value="user.id">{{ user.name }}</option>
+          </select>
+
+          <ul v-if="selectedUser">
+            <li v-for="post in userPosts" :key="post.id">
+              {{ post.title }}
+            </li>
+          </ul>
         </div>
       </div>
-      <ul class="list-group list-group-flush list-group-numbered">
-        <li class="list-group-item" v-for="todo in filteredTodos" :key="todo">
-          <input class="form-check-input" type="checkbox" v-model="todo.done" id="flexCheckDefault">
-          <label class="form-check-label ms-3" for="flexCheckDefault" :class="{ done: todo.done }">
-            {{todo.title}}
-          </label>
-          <button @click="removeTodo(todo)" class="btn btn-danger btn-sm float-end" type="button" name="button">
-            <i class="bi bi-trash-fill"></i>
-          </button>
-        </li>
-      </ul>
     </div>
   </div>
 </template>
@@ -62,7 +80,11 @@ export default {
   data: () => ({
     newTodo: '',
     todos: [],
-    filter: null
+    filter: null,
+    selectedMenu: 'Todos', // Default menu selection
+    users: [], // Placeholder for users data
+    selectedUser: null, // Selected user ID for posts
+    userPosts: [] // Placeholder for user posts data
   }),
 
   methods: {
@@ -77,14 +99,6 @@ export default {
       const todoIndex = this.todos.indexOf(todo);
       this.todos.splice(todoIndex, 1);
     },
-    allDone() {
-      this.todos.forEach(todo => {
-        todo.done = true;
-      });
-    },
-    allRemove() {
-      this.todos = [];
-    },
     toggleFilter(type) {
       if (this.filter === type) {
         this.filter = null;
@@ -94,8 +108,31 @@ export default {
     },
     clearFilter() {
       this.filter = null;
+    },
+    fetchUsers() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => this.users = data)
+        .catch(error => console.error('Error fetching users:', error));
+    },
+    fetchUserPosts(userId) {
+      fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+        .then(response => response.json())
+        .then(data => this.userPosts = data)
+        .catch(error => console.error(`Error fetching posts for user ${userId}:`, error));
     }
   },
+  watch: {
+    selectedUser(newVal) {
+      if (newVal) {
+        this.fetchUserPosts(newVal);
+      }
+    }
+  },
+  created() {
+    this.fetchUsers();
+  },
+
   computed: {
     filteredTodos() {
       if (this.filter === 'done') {
